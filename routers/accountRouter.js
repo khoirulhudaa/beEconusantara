908eff825343af39e2a3e47ee68e300a06da94ae
valclassdevelop <developervalclass.com>
2024-04-09 10:16:48 +0700

    backend ecoNusantara

--- routers/accountRouter.js
+++ routers/accountRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 
-router.post('/masuk', authController.signin)
-router.post('/daftar', authController.signup)
+router.post('/signin', authController.signin)
+router.post('/signup', authController.signup)
 
 module.exports = router
