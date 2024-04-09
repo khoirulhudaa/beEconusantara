25171591bfa33e3d58fbc81285dbf2b1f027bc15
valclassdevelop <developervalclass.com>
2024-04-09 10:12:30 +0700

    backend ecoNusantara

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -11,7 +11,7 @@ module.exports = async (req, res, next) => {
         });
     }
 
-    jwt.verify(token, 'geospasial', function (error) {
+    jwt.verify(token, 'ecoNusantara', function (error) {
         if (error) {
             return res.status(403).json({
                 status: false,
